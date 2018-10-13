40811de1b78a6c3dc63e9938e83efcd1e2e84e90
Seanhhe <seanhhe@gmail.com>
2018-10-13 09:00:13 +0800

    20181007_MyLottery

---
+++ app/src/main/java/tw/sean/mylottery/CupActivity.java
@@ -0,0 +1,27 @@
+package tw.sean.mylottery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class CupActivity extends AppCompatActivity {
+    private ImageView imgCup;
+    private int[] cups = {
+            R.drawable.cup0, R.drawable.cup1,
+            R.drawable.cup2, R.drawable.cup2
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cup);
+
+        imgCup = findViewById(R.id.cupImg);
+
+        int rand = (int)(Math.random()*4);
+        imgCup.setImageResource(R.drawable.cups[rand]);
+
+        setResult(intent);
+    }
+}
